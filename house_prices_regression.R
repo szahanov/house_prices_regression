f98b06004e9e4d020ee3f9af170f252faaf9bcbe
Steven <steven@zahanov.com>
2017-11-05 18:47:26 -0500

    got a decent model with more decision trees

--- house_prices_regression.R
+++ house_prices_regression.R
@@ -77,6 +77,7 @@ dataset <- transform(dataset,
 	YearRemodAdd = as.numeric(YearRemodAdd),
 	BsmtFinSF1 = as.numeric(BsmtFinSF1),
 	BsmtUnfSF = as.numeric(BsmtUnfSF),
+	TotalBsmtSF = as.numeric(TotalBsmtSF),	# Added back to replace the above two
 	X1stFlrSF = as.numeric(X1stFlrSF),
 	X2ndFlrSF = as.numeric(X2ndFlrSF),
 	BsmtFullBath = as.numeric(BsmtFullBath),
@@ -111,9 +112,9 @@ x <- sapply(x, as.numeric)
 # datasubset <- matrix(c(dataset$SalePrice,dataset$X1stFlrSF,dataset$X2ndFlrSF),nrow=length(dataset$SalePrice))
 #datasubset <- dataset[,c("SalePrice","MSZoning","X1stFlrSF","X2ndFlrSF")]
 #colnames(datasubset) <- c("SalePrice","MSZoning","X1stFlrSF","X2ndFlrSF")
-dataset <- dataset[,c("SalePrice",	"MSSubClass",	"LotArea",	"OverallQual",	"OverallCond",
-"YearBuilt",	"YearRemodAdd",	"BsmtFinSF1",	"BsmtUnfSF",	"X1stFlrSF",	"X2ndFlrSF",	"BsmtFullBath",	"BsmtHalfBath",	"FullBath",	"HalfBath",	"BedroomAbvGr",	"KitchenAbvGr",	"GarageCars",	"WoodDeckSF",	"OpenPorchSF",	"EnclosedPorch",	"X3SsnPorch",	"ScreenPorch",	"PoolArea",	"YrSold")]
-colnames(dataset) <- c("SalePrice",	"MSSubClass",	"LotArea",	"OverallQual",	"OverallCond",	"YearBuilt",	"YearRemodAdd",	"BsmtFinSF1",	"BsmtUnfSF",	"X1stFlrSF",	"X2ndFlrSF",	"BsmtFullBath",	"BsmtHalfBath",	"FullBath",	"HalfBath",	"BedroomAbvGr",	"KitchenAbvGr",	"GarageCars",	"WoodDeckSF",	"OpenPorchSF",	"EnclosedPorch",	"X3SsnPorch",	"ScreenPorch",	"PoolArea",	"YrSold")
+dataset1 <- dataset[,c("SalePrice",	"MSSubClass",	"LotArea",	"OverallQual",	"OverallCond",
+"YearBuilt",	"YearRemodAdd",	"BsmtFinSF1",	"BsmtUnfSF",	"TotalBsmtSF",	"X1stFlrSF",	"X2ndFlrSF",	"BsmtFullBath",	"BsmtHalfBath",	"FullBath",	"HalfBath",	"BedroomAbvGr",	"KitchenAbvGr",	"GarageCars",	"WoodDeckSF",	"OpenPorchSF",	"EnclosedPorch",	"X3SsnPorch",	"ScreenPorch",	"PoolArea",	"YrSold")]
+colnames(dataset1) <- c("SalePrice",	"MSSubClass",	"LotArea",	"OverallQual",	"OverallCond",	"YearBuilt",	"YearRemodAdd",	"BsmtFinSF1",	"BsmtUnfSF",	"TotalBsmtSF",	"X1stFlrSF",	"X2ndFlrSF",	"BsmtFullBath",	"BsmtHalfBath",	"FullBath",	"HalfBath",	"BedroomAbvGr",	"KitchenAbvGr",	"GarageCars",	"WoodDeckSF",	"OpenPorchSF",	"EnclosedPorch",	"X3SsnPorch",	"ScreenPorch",	"PoolArea",	"YrSold")
 
 
 # https://www.analyticsvidhya.com/blog/2016/01/xgboost-algorithm-easy-steps/
@@ -121,32 +122,38 @@ colnames(dataset) <- c("SalePrice",	"MSSubClass",	"LotArea",	"OverallQual",	"Ove
 # xgb <- xgboost(data = data.matrix(dataset[,-1]),  label = y,  eta = 0.1, max_depth = 15,  nround=25,  subsample = 0.5, colsample_bytree = 0.5, seed = 1, eval_metric = "merror", objective = "multi:softprob", num_class = 12, nthread = 3)
 
 
-output_vector <- dataset[,1]
+output_vector <- dataset1[,1]
 
-xgb <- xgboost(data = data.matrix(dataset[,-1]),  label = output_vector,  eta = 0.1, max_depth = 15,  nround=25)
+# xgb <- xgboost(data = data.matrix(dataset1[,-1]),  label = output_vector,  eta = 0.1, max_depth = 15,  nround=25)
+xgb <- xgboost(data = data.matrix(dataset1[,-1]),  label = output_vector,  eta = 0.3, max_depth = 6,  nround=100)
 
 # predict values in test set
-xgb_predictions <- predict(xgb, data.matrix(dataset[,-1]))
+xgb_predictions <- predict(xgb, data.matrix(dataset1[,-1]))
 
-plot(xgb_predictions,dataset$SalePrice)
+plot(xgb_predictions,dataset1$SalePrice)
 
 # MSE
-sum((dataset$SalePrice - xgb_predictions)^2)
+sum((dataset1$SalePrice - xgb_predictions)^2)
 # AVG - test mean square error
-mse_test_value <- mean((dataset$SalePrice - xgb_predictions)^2)
+mse_test_value <- mean((dataset1$SalePrice - xgb_predictions)^2)
 
 # some spot checks
-i=5;(xgb_predictions[i] - dataset$SalePrice[i])/dataset$SalePrice[i]
-i=20;(xgb_predictions[i] - dataset$SalePrice[i])/dataset$SalePrice[i]
-i=35;(xgb_predictions[i] - dataset$SalePrice[i])/dataset$SalePrice[i]
-plot((xgb_predictions - dataset$SalePrice)/dataset$SalePrice)
+i=5;(xgb_predictions[i] - dataset1$SalePrice[i])/dataset1$SalePrice[i]
+i=20;(xgb_predictions[i] - dataset1$SalePrice[i])/dataset1$SalePrice[i]
+i=35;(xgb_predictions[i] - dataset1$SalePrice[i])/dataset1$SalePrice[i]
+plot(xgb_predictions/max(dataset1$SalePrice),dataset1$SalePrice/max(dataset1$SalePrice))
+plot((xgb_predictions - dataset1$SalePrice)/dataset1$SalePrice)
 
+
+
+# nrounds: the number of decision trees in the final model
+# objective: the training objective to use, where “binary:logistic” means a binary classifier.
 
 
 
 
 # Look at variable importance
-X <- dataset
+X <- dataset1
 # Lets start with finding what the actual tree looks like
 model <- xgb.dump(xgb, with.stats = T)
 model[1:10] #This statement prints top 10 nodes of the model
@@ -155,18 +162,72 @@ names <- dimnames(data.matrix(X[,-1]))[[2]]
 # Compute feature importance matrix
 importance_matrix <- xgb.importance(names, model = xgb)
 # Nice graph
-xgb.plot.importance(importance_matrix[1:10,])
+xgb.plot.importance(importance_matrix[1:20,])
 #In case last step does not work for you because of a version issue, you can try following :
 barplot(importance_matrix[,1])
 
 
+# results: overallqual, garagecars, x1st, x2nd, bsmtfin, fullbath, lotarea, yearremodeled, openporch, yearbuilt, overallcond, halfbath, bsmtunf, mssubclass, wooddeck
+
+# unfinished basement space correlated well, using total basement sf instead because it makes more sense
+# halfbath correlated decently well too but it doesn't make as much sense as a total bathroom count; actually since both total bathroom and halfbath correlated then maybe it makes sense to keep them both
+
+
+# Second try
+dataset2 <- dataset[,c("SalePrice", "MSSubClass", "LotArea", "OverallQual", "OverallCond",
+"YearBuilt", "YearRemodAdd", "TotalBsmtSF", "X1stFlrSF","X2ndFlrSF","FullBath",	"HalfBath", "GarageCars", "WoodDeckSF",	"OpenPorchSF")]
+colnames(dataset2) <- c("SalePrice", "MSSubClass", "LotArea", "OverallQual", "OverallCond",
+"YearBuilt", "YearRemodAdd", "TotalBsmtSF", "X1stFlrSF","X2ndFlrSF","FullBath",	"HalfBath", "GarageCars", "WoodDeckSF",	"OpenPorchSF")
+
+# Second try xgboost model
+
+output_vector2 <- dataset2[,1]
+
+xgb2 <- xgboost(data = data.matrix(dataset2[,-1]),  label = output_vector,  eta = 0.1, max_depth = 15,  nround=25)
+
+# predict values in test set
+xgb_predictions2 <- predict(xgb2, data.matrix(dataset2[,-1]))
+
+plot(xgb_predictions2,dataset2$SalePrice)
+
+# MSE
+sum((dataset2$SalePrice - xgb_predictions2)^2)
+# AVG - test mean square error
+mse_test_value2 <- mean((dataset2$SalePrice - xgb_predictions2)^2)
+
+# some spot checks
+i=5;(xgb_predictions2[i] - dataset2$SalePrice[i])/dataset2$SalePrice[i]
+i=20;(xgb_predictions2[i] - dataset2$SalePrice[i])/dataset2$SalePrice[i]
+i=35;(xgb_predictions2[i] - dataset2$SalePrice[i])/dataset2$SalePrice[i]
+plot((xgb_predictions2 - dataset2$SalePrice)/dataset2$SalePrice)
+
+
+# /Second try xgboost model
+
+# Second try was worse
 
 
 
+# Second try look at variable importance
+
+X <- dataset2
+# Lets start with finding what the actual tree looks like
+model <- xgb.dump(xgb, with.stats = T)
+model[1:10] #This statement prints top 10 nodes of the model
+# Get the feature real names
+names <- dimnames(data.matrix(X[,-1]))[[2]]
+# Compute feature importance matrix
+importance_matrix <- xgb.importance(names, model = xgb)
+# Nice graph
+xgb.plot.importance(importance_matrix[1:20,])
+#In case last step does not work for you because of a version issue, you can try following :
+barplot(importance_matrix[,1])
+
+# /Second try look at variable importance
 
 
-
-
+# Check for overfitting and do general testing
+# https://machinelearningmastery.com/avoid-overfitting-by-early-stopping-with-xgboost-in-python/
 
 
 ## Random Forest model for comparison
